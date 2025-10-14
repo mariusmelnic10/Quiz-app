aac841d9bf921ad771a53918421d190a03340077
Marius Melnic <mariusmelnic10@gmail.com>
2025-10-14 21:56:04 +0200

    quizapp.js

--- quizapp.js
+++ quizapp.js
@@ -2,24 +2,30 @@ const answerbtn1 = document.getElementById("answer1");
 const answerbtn2 = document.getElementById("answer2");
 const answerbtn3 = document.getElementById("answer3");
 const answerbtn4 = document.getElementById("answer4");
+const skip = document.getElementById("skipbtn");
 
 answerbtn1.addEventListener("click", listen1)
 answerbtn2.addEventListener("click", listen2)
 answerbtn3.addEventListener("click", listen3)
 answerbtn4.addEventListener("click", listen4)
+skip.addEventListener("click", skip1)
 
 function listen1() {
-    console.log("first button was used")
+    console.log("red button was used")
 };
 
 function listen2() {
-    console.log("second button was used")
+    console.log("blue button was used")
 };
 
 function listen3() {
-    console.log("third button was used")
+    console.log("yellow button was used")
 };
 
 function listen4() {
-    console.log("fourth button was used")
-};
+    console.log("green button was used")
+};      
+
+function skip1() {
+    console.log("You just skipped")
+}   
